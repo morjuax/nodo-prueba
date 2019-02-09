a2f1709def3f4e755bec76ba0d875fb036cf2525
Juan Moreno <morjua19@gmail.com>
2019-02-09 12:47:36 -0500

    commit 1er Desafio JS es6 ready..!

--- P1/logic-test.js
+++ P1/logic-test.js
@@ -1,62 +1,62 @@
 // No editar
 const teams = [
-  { id: 1, country: 'Spain', name: 'Real Madrid C.F.' },
-  { id: 2, country: 'Italy', name: 'A.C. Milan' },
-  { id: 3, country: 'Spain', name: 'Futbol Club Barcelona' },
-  { id: 4, country: 'Germany', name: 'FC Bayern Munich' },
-  { id: 5, country: 'England', name: 'Liverpool F.C.' },
-  { id: 6, country: 'Netherlands', name: 'AFC Ajax' },
-  { id: 7, country: 'Italy', name: 'Inter Milan' },
-  { id: 8, country: 'England', name: 'Manchester United F.C.' },
-  { id: 9, country: 'England', name: 'Chelsea F.C.' },
-  { id: 10, country: 'Portugal', name: 'FC Porto' },
-  { id: 11, country: 'Germany', name: 'Borussia Dortmund' },
-  { id: 12, country: 'Italy', name: 'Juventus FC' },
-  { id: 13, country: 'France', name: 'Olympique Marseille' }
+    {id: 1, country: 'Spain', name: 'Real Madrid C.F.'},
+    {id: 2, country: 'Italy', name: 'A.C. Milan'},
+    {id: 3, country: 'Spain', name: 'Futbol Club Barcelona'},
+    {id: 4, country: 'Germany', name: 'FC Bayern Munich'},
+    {id: 5, country: 'England', name: 'Liverpool F.C.'},
+    {id: 6, country: 'Netherlands', name: 'AFC Ajax'},
+    {id: 7, country: 'Italy', name: 'Inter Milan'},
+    {id: 8, country: 'England', name: 'Manchester United F.C.'},
+    {id: 9, country: 'England', name: 'Chelsea F.C.'},
+    {id: 10, country: 'Portugal', name: 'FC Porto'},
+    {id: 11, country: 'Germany', name: 'Borussia Dortmund'},
+    {id: 12, country: 'Italy', name: 'Juventus FC'},
+    {id: 13, country: 'France', name: 'Olympique Marseille'}
 
-]
+];
 
 const leagues = [
-  { id: 1, country: 'England', name: 'Premier League' },
-  { id: 2, country: 'Germany', name: 'Bundesliga' },
-  { id: 3, country: 'Netherlands', name: 'Eredivisie' },
-  { id: 4, country: 'Spain', name: 'La Liga' },
-  { id: 5, country: 'Italy', name: 'Serie A' },
-  { id: 6, country: 'Portugal', name: 'Liga NOS' },
-  { id: 7, country: 'France', name: 'Lige 1' }
-]
+    {id: 1, country: 'England', name: 'Premier League'},
+    {id: 2, country: 'Germany', name: 'Bundesliga'},
+    {id: 3, country: 'Netherlands', name: 'Eredivisie'},
+    {id: 4, country: 'Spain', name: 'La Liga'},
+    {id: 5, country: 'Italy', name: 'Serie A'},
+    {id: 6, country: 'Portugal', name: 'Liga NOS'},
+    {id: 7, country: 'France', name: 'Lige 1'}
+];
 
 const teamsByLeague = [
-  { teamId: 12, leagueId: 5 },
-  { teamId: 6, leagueId: 3 },
-  { teamId: 2, leagueId: 5 },
-  { teamId: 3, leagueId: 4 },
-  { teamId: 4, leagueId: 2 },
-  { teamId: 8, leagueId: 1 },
-  { teamId: 10, leagueId: 6 },
-  { teamId: 5, leagueId: 1 },
-  { teamId: 7, leagueId: 5 },
-  { teamId: 9, leagueId: 1 },
-  { teamId: 11, leagueId: 2 },
-  { teamId: 1, leagueId: 4 },
-  { teamId: 13, leagueId: 7 }
-]
+    {teamId: 12, leagueId: 5},
+    {teamId: 6, leagueId: 3},
+    {teamId: 2, leagueId: 5},
+    {teamId: 3, leagueId: 4},
+    {teamId: 4, leagueId: 2},
+    {teamId: 8, leagueId: 1},
+    {teamId: 10, leagueId: 6},
+    {teamId: 5, leagueId: 1},
+    {teamId: 7, leagueId: 5},
+    {teamId: 9, leagueId: 1},
+    {teamId: 11, leagueId: 2},
+    {teamId: 1, leagueId: 4},
+    {teamId: 13, leagueId: 7}
+];
 
 const winsByTeams = [
-  { teamId: 10, wins: 2 },
-  { teamId: 6, wins: 4 },
-  { teamId: 5, wins: 5 },
-  { teamId: 1, wins: 13 },
-  { teamId: 7, wins: 3 },
-  { teamId: 4, wins: 5 },
-  { teamId: 8, wins: 3 },
-  { teamId: 2, wins: 7 },
-  { teamId: 9, wins: 1 },
-  { teamId: 3, wins: 5 },
-  { teamId: 11, wins: 1 },
-  { teamId: 12, wins: 2 },
-  { teamId: 13, wins: 1 }
-]
+    {teamId: 10, wins: 2}, //FC Porto
+    {teamId: 6, wins: 4}, //AFC Ajax
+    {teamId: 5, wins: 5}, //Liverpool F.C. *
+    {teamId: 1, wins: 13}, //Real Madrid C.F.
+    {teamId: 7, wins: 3}, //Inter Milan **
+    {teamId: 4, wins: 5}, //FC Bayern Munich
+    {teamId: 8, wins: 3}, //Manchester United F.C. *
+    {teamId: 2, wins: 7}, //A.C. Milan **
+    {teamId: 9, wins: 1}, //Chelsea F.C. *
+    {teamId: 3, wins: 5}, //Futbol Club Barcelona
+    {teamId: 11, wins: 1}, //Borussia Dortmund
+    {teamId: 12, wins: 2}, //Juventus FC **
+    {teamId: 13, wins: 1} //Olympique Marseille
+];
 
 /*
   SECCIÓN PROBLEMAS
@@ -68,85 +68,212 @@ const winsByTeams = [
     - Su prueba debe ejecutarse sin errores con: node logic-test.js
 */
 
-// 0 Arreglo con los ids de los equipos (función de ejemplo)
-function listTeamsIds () {
-  return teams.map((client) => client.id)
+// 0) Arreglo con los ids de los equipos (función de ejemplo)
+function listTeamsIds() {
+    return teams.map((client) => client.id)
 }
 
-// 1 Arreglo con los nombres de los equipos y el país al que pertenecen, ordenados alfabéticamente por el nombre de su país de origen.
-function listTeamsByCountry () {
-  // CODE HERE
+// 1) Arreglo con los nombres de los equipos y el país al que pertenecen, ordenados alfabéticamente por el nombre de su país de origen.
+function listTeamsByCountry() {
+    return teams.sort(_compareCountry).map(teamObj => ({name: teamObj.name, country: teamObj.country}));
 }
 
-// 2 Arreglo con los nombres de los equipos ordenados de mayor a menor por la cantidad de victorias en champions league.
-function sortTeamsByWins () {
-  // CODE HERE
+function _compareCountry(obj1, obj2) {
+    let country1 = obj1.country.toLowerCase(), country2 = obj2.country.toLowerCase();
+    if (country1 < country2) return -1;
+    if (country1 > country2) return 1;
+    return 0;
 }
 
-// 3 Arreglo de objetos en donde se muestre el nombre de las ligas y la sumatoria de las victorias de los equipos que pertenecen a ellas.
-function leaguesWithWins () {
-  // CODE HERE
+// 2) Arreglo con los nombres de los equipos ordenados de mayor a menor por la cantidad de victorias en champions league.
+function sortTeamsByWins() {
+    let teamOrderedByWins = winsByTeams.sort((a, b) => {
+        return _compareNumber(a, b, 'DESC', 'wins');
+    });
+
+    let winsByTeamsOrdered = [];
+    teamOrderedByWins.forEach((teamWin) => {
+        let team = teams.find(team => team.id === teamWin.teamId);
+        winsByTeamsOrdered.push({name: team.name, wins: teamWin.wins});
+    });
+    return winsByTeamsOrdered;
 }
 
-// 4 Objeto en que las claves sean los nombres de las ligas y los valores el nombre del equipo con la menor cantidad de victorias en champions.
-function leaguesWithTeamWithLestWins () {
-  // CODE HERE
+//####
+function _compareNumberWins(obj1, obj2, orderBy) { //sort  //deprecated
+    if (orderBy === 'ASC') return obj1.wins - obj2.wins;
+    if (orderBy === 'DESC') return obj2.wins - obj1.wins;
+    return 0;
 }
+
+function _compareNumber(obj1, obj2, orderBy, nameProperty) { //sort
+    let valObj1 = obj1[nameProperty];
+    let valObj2 = obj2[nameProperty];
+    if (orderBy === 'ASC') return valObj1 - valObj2;
+    if (orderBy === 'DESC') return valObj2 - valObj1;
+    return 0;
+}
+
+//####
+
+// 3) Arreglo de objetos en donde se muestre el nombre de las ligas y la sumatoria de las victorias de los equipos que pertenecen a ellas.
+function leaguesWithWins() {
+    let winsByLeague = [];
+    leagues.forEach((league) => {
+        let teamsByLeagues = teamsByLeague.filter(tbl => tbl.leagueId === league.id);
+        let accumWinsByLeague = 0;
+        teamsByLeagues.forEach((tbl) => {
+            let winsByTeam = winsByTeams.find(team => team.teamId === tbl.teamId);
+            accumWinsByLeague += winsByTeam.wins;
+        });
+        winsByLeague.push({name: league.name, wins: accumWinsByLeague});
+    });
+    return winsByLeague;
+}
+
+// 4) Objeto en que las claves sean los nombres de las ligas y los valores el nombre del equipo con la menor cantidad de victorias en champions.
+function leaguesWithTeamWithLestWins() {
+    let teamLestWinsByLeague = [];
+    leagues.forEach((league) => {
+        let teamsByLeagues = teamsByLeague.filter(tbl => tbl.leagueId === league.id);
+        teamLestWinsByLeague.push({[_snakeCase(league.name)]: _getMinTeam(_winsByTeamByLeague(teamsByLeagues, league))});
+    });
+    return teamLestWinsByLeague;
+}
+
+//####
+function _getMinTeam(data) {
+    // return data.reduce((min, p) => (p.wins < min) ? p.wins : min, data[0].wins);
+    let team = data[0].team;
+    data.reduce((min, p) => {
+        if (p.wins < min) {
+            team = p.team;
+            return p.wins;
+        }
+        return min;
+
+    }, data[0].wins);
+
+    return team;
+}
+
+function _getMaxTeam(data) {
+    let team = data[0].team;
+    data.reduce((min, p) => {
+        if (p.wins > min) {
+            team = p.team;
+            return p.wins;
+        }
+        return min;
+
+    }, data[0].wins);
+
+    return team;
+}
+
+function _snakeCase(str) {
+    return str.toLowerCase().replace(' ', '_');
+}
+
+function _winsByTeamByLeague(teamsByLeagues, league) {
+    let winsByTeamByLeague = [];
+    teamsByLeagues.forEach((tbl) => {
+        let winsByTeam = winsByTeams.find(team => team.teamId === tbl.teamId);
+        let team = teams.find(team => team.id === tbl.teamId);
+        winsByTeamByLeague.push({
+            league: league.name,
+            team: team.name,
+            wins: winsByTeam.wins,
+        });
+    });
+    return winsByTeamByLeague;
+}
+
+//####
 
 // 5 Objeto en que las claves sean los nombres de las ligas y los valores el nombre del equipo con la mayor cantidad de victorias en champions.
-function leaguesWithTeamWithMostWins () {
-  // CODE HERE
+function leaguesWithTeamWithMostWins() {
+    let teamMostWinsByLeague = [];
+    leagues.forEach((league) => {
+        let teamsByLeagues = teamsByLeague.filter(tbl => tbl.leagueId === league.id);
+        teamMostWinsByLeague.push({[_snakeCase(league.name)]: _getMaxTeam(_winsByTeamByLeague(teamsByLeagues, league))});
+    });
+    return teamMostWinsByLeague;
 }
 
-// 6 Arreglo con los nombres de las ligas ordenadas de mayor a menor por la cantidad de victorias de sus equipos.
-function sortLeaguesByTeamsByWins () {
-  // CODE HERE
+// 6) Arreglo con los nombres de las ligas ordenadas de mayor a menor por la cantidad de victorias de sus equipos.
+function sortLeaguesByTeamsByWins() {
+    let leaguesOrderedByWins = [];
+    leagues.forEach((league) => {
+        let teamsByLeagues = teamsByLeague.filter(tbl => tbl.leagueId === league.id);
+        let totalWinsByLeague = _winsByTeamByLeague(teamsByLeagues, league).reduce((min, p) => (min + p.wins), 0);
+        leaguesOrderedByWins.push({name: league.name, wins: totalWinsByLeague});
+    });
+    return leaguesOrderedByWins.sort((a, b) => {
+        return _compareNumber(a, b, 'DESC', 'wins');
+    }).map(league => ({name: league.name}));
 }
 
-// 7 Arreglo con los nombres de las ligas ordenadas de mayor a menor por la cantidad de equipos que participan en ellas.
-function sortLeaguesByTeams () {
-  // CODE HERE
+// 7) Arreglo con los nombres de las ligas ordenadas de mayor a menor por la cantidad de equipos que participan en ellas.
+function sortLeaguesByTeams() {
+    let leaguesOrderedByTeams = [];
+    leagues.forEach((league) => {
+        let teamsByLeagues = teamsByLeague.filter(tbl => tbl.leagueId === league.id).length;
+        leaguesOrderedByTeams.push({name: league.name, quality: teamsByLeagues});
+    });
+    return leaguesOrderedByTeams.sort((a, b) => {
+        return _compareNumber(a, b, 'DESC', 'quality');
+    });
 }
 
-// 8 Agregar un nuevo equipo con datos ficticios a "teams", asociarlo a la liga de Francia y agregar un total de 4 victorias en champions.
+// 8) Agregar un nuevo equipo con datos ficticios a "teams", asociarlo a la liga de Francia y agregar un total de 4 victorias en champions.
 // Luego devolver el lugar que ocupa este equipo en el ranking de la pregunta 2.
 // No modificar arreglos originales para no alterar las respuestas anteriores al correr la solución
-function newTeamRanking () {
-  // CODE HERE
+function newTeamRanking() {
+    teams.push({id: 14, country: 'France', name: 'Paris Saint Germain'}); //new team
+    teamsByLeague.push({teamId: 14, leagueId: 7}); //new teamByLeague
+    winsByTeams.push({teamId: 14, wins: 4});
+    return `The Paris Saint Germain  is in the position:  ${sortTeamsByWins().findIndex(obj => (obj.name === 'Paris Saint Germain')) + 1}`;
 }
 
-// 9 Realice una función que retorne una promesa con los nombres de los equipos en upper case.
+// 9) Realice una función que retorne una promesa con los nombres de los equipos en upper case.
 // haga la llamada a la función creada desde esta función y asignarle la respuesta a la variable response.
 // recuerde que debe esperar el retorno de función asíncrona para que su resultado pueda ser mostrado por el
 // console.log. Utilice async await para la llamada asíncrona a la función.
 // NOTA: solo debe crear la función asíncrona y agregar la llamada en la siguiente función.
-async function getTeamsNamesAsUpperCase () {
-  let response
-  // ------MAKE AWAIT CALL HERE------
+async function getTeamsNamesAsUpperCase() {
+    let response;
+    // ------MAKE AWAIT CALL HERE------
+    response = await _promiseNamesTeamsUC();
+    // --------------------------------
+    console.log('response:');
+    console.log(response);
+}
 
-  // --------------------------------
-  console.log('response:')
-  console.log(response)
+async function _promiseNamesTeamsUC() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(teams.map(team => (team.name.toUpperCase()))), 3000);
+    });
 }
 
 // Impresión de soluciones. No modificar.
-console.log('Pregunta 0')
-console.log(listTeamsIds())
-console.log('Pregunta 1')
-console.log(listTeamsByCountry())
-console.log('Pregunta 2')
-console.log(sortTeamsByWins())
-console.log('Pregunta 3')
-console.log(leaguesWithWins())
-console.log('Pregunta 4')
-console.log((leaguesWithTeamWithLestWins()))
-console.log('Pregunta 5')
-console.log((leaguesWithTeamWithMostWins()))
-console.log('Pregunta 6')
-console.log((sortLeaguesByTeamsByWins()))
-console.log('Pregunta 7')
-console.log((sortLeaguesByTeams()))
-console.log('Pregunta 8')
-console.log((newTeamRanking()))
-console.log('Pregunta 9')
-console.log(getTeamsNamesAsUpperCase())
+console.log('Pregunta 0');
+console.log(listTeamsIds());
+console.log('Pregunta 1');
+console.log(listTeamsByCountry());
+console.log('Pregunta 2');
+console.log(sortTeamsByWins());
+console.log('Pregunta 3');
+console.log(leaguesWithWins());
+console.log('Pregunta 4');
+console.log((leaguesWithTeamWithLestWins()));
+console.log('Pregunta 5');
+console.log((leaguesWithTeamWithMostWins()));
+console.log('Pregunta 6');
+console.log((sortLeaguesByTeamsByWins()));
+console.log('Pregunta 7');
+console.log((sortLeaguesByTeams()));
+console.log('Pregunta 8');
+console.log((newTeamRanking()));
+console.log('Pregunta 9');
+console.log(getTeamsNamesAsUpperCase());
